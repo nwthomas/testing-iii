42194941dd85d94a67554cacee875209ab96e7f2
Nathan Thomas <nwthomas@me.com>
2019-03-06 14:08:31 -0800

    Finish 4 tests in Dashboard.test.js

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,28 @@
-// Test away
+import React from "react";
+import { render } from "react-testing-library";
+import renderer from "react-test-renderer";
+import "react-testing-library/cleanup-after-each";
+import "jest-dom/extend-expect";
+
+import Dashboard from "./Dashboard";
+
+describe("<Dashboard />", () => {
+  it("renders the Dashboard component without crashing", () => {
+    const helpers = render(<Dashboard />);
+  });
+
+  it("matches the snapshot of Dashboard", () => {
+    const tree = renderer.create(<Dashboard />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  it("renders the Display component to the screen as a child component", () => {
+    const { getByText } = render(<Dashboard />);
+    expect(getByText("Unlocked")).toBeTruthy(); // Checks if the default is unlocked in the display
+  });
+
+  it("renders the Controls component to the screen as a child component", () => {
+    const { getByText } = render(<Dashboard />);
+    expect(getByText("Close Gate")).toBeTruthy(); // Checks if the default is an opened gate button
+  });
+});
